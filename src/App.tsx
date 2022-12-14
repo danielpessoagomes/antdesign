29fd207e999d3daafae8047035c3c99edca25075
Daniel Gomes <danielgomes@dginfope.com.br>
2022-12-13 23:05:32 -0300

    12.10. Tipografia

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import { Col, Row } from 'antd';
+import { Col, Row, Typography } from 'antd';
+import { useState } from 'react';
 import './index.css';
 
+const { Title, Paragraph } = Typography;
+
 function App() {
+  const [editableStr, setEditableStr] = useState(
+    'This is an editable text.'
+  );
+
+  const [ellipsis, setEllipsis] = useState(true);
+
   return (
     <div>
       <Row gutter={8} justify={'center'}>
-        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
-          <div className={'Column'}> Coluna 1</div>
+        <Col span={12}>
+          <Title>Título de nível 1</Title>
+          <Title level={2}>Título de nível 2</Title>
+          <Title level={3}>Título de nível 3</Title>
+          <Title level={4}>Título de nível 4</Title>
+          <Title level={5}>Título de nível 5</Title>
         </Col>
-        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
-          <div className={'Column'}> Coluna 2</div>
-        </Col>
-        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
-          <div className={'Column'}> Coluna 3</div>
-        </Col>
-        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
-          <div className={'Column'}> Coluna 4</div>
-        </Col>
-        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
-          <div className={'Column'}> Coluna 5</div>
+        <Col span={12}>
+          <Paragraph
+            editable={{ onChange: setEditableStr }}
+          >
+            {editableStr}
+          </Paragraph>
+          <Paragraph ellipsis={{ rows: 3 }}>
+            Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Perferendis iste odio qui
+            voluptas temporibus neque ex modi vero corrupti
+            totam dolorem consectetur dolores porro saepe
+            cumque velit ad commodi fuga ipsam, rerum dolor
+            sunt laborum? Temporibus, saepe unde ratione,
+            laudantium a nihil dolor id architecto
+            consequuntur atque, voluptas labore. Eaque
+            repudiandae repellendus dolores impedit magni
+            cupiditate velit eligendi quaerat. Nemo,
+            delectus maiores dicta ex fugit soluta beatae
+            praesentium porro voluptates molestiae nobis
+            voluptate impedit quibusdam temporibus.
+            Exercitationem tempora nulla molestiae possimus
+            explicabo nesciunt culpa esse, aliquam
+            necessitatibus quae error cum eius reiciendis,
+            est asperiores beatae porro eveniet dolores.
+            Officia, nostrum!
+          </Paragraph>
         </Col>
       </Row>
     </div>
