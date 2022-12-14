2b63fd7ba74c43791375d790e566bcf010ce4bc0
Daniel Gomes <danielgomes@dginfope.com.br>
2022-12-13 23:43:29 -0300

    12.12. Introdução ao Table

---
+++ sdk/services/Metric.service.ts
@@ -0,0 +1,40 @@
+import { Metric } from "../@types";
+import Service from "../Service";
+
+class MetricService extends Service {
+  static getTop3Tags() {
+    return this.Http.get<Metric.EditorTagRatio>(
+      "/metrics/editor/top3-tags"
+    ).then(this.getData);
+  }
+
+  static getEditorMonthlyEarnings() {
+    return this.Http.get<Metric.EditorMonthlyEarnings>(
+      "/metrics/editor/monthly-earnings"
+    ).then(this.getData);
+  }
+
+  static getMonthlyRevenuesExpenses() {
+    return this.Http.get<Metric.MonthlyRevenuesExpenses>(
+      "/metrics/monthly-revenues-expenses",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(this.getData);
+  }
+
+  static getMonthlyRevenuesExpensesChartJs() {
+    return this.Http.get<Metric.MonthlyRevenuesExpenses>(
+      "/metrics/monthly-revenues-expenses",
+      {
+        headers: {
+          "Content-Type": "application/vnd.alganews.charjs+json",
+        },
+      }
+    ).then(this.getData);
+  }
+}
+
+export default MetricService;
--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,33 @@
-import { Col, Row, Typography } from 'antd';
-import { useState } from 'react';
+import { Col, Row, Table } from 'antd';
+import { useEffect, useState } from 'react';
 import './index.css';
+import { User } from './../sdk/@types/User';
+import UserService from './../sdk/services/User.service';
 
-const { Title, Paragraph } = Typography;
+function App(): JSX.Element {
+  const [users, setUsers] = useState<User.Summary[]>([]);
 
-function App() {
-  const [editableStr, setEditableStr] = useState(
-    'This is an editable text.'
-  );
+  useEffect(() => {
+    UserService.getAllUsers().then(setUsers);
+  }, []);
 
   return (
     <div>
       <Row gutter={8} justify={'center'}>
-        <Col span={12}>
-          <Title>Título de nível 1</Title>
-          <Title level={2}>Título de nível 2</Title>
-          <Title level={3}>Título de nível 3</Title>
-          <Title level={4}>Título de nível 4</Title>
-          <Title level={5}>Título de nível 5</Title>
-        </Col>
-        <Col span={12}>
-          <Paragraph
-            editable={{ onChange: setEditableStr }}
-          >
-            {editableStr}
-          </Paragraph>
-          <Paragraph ellipsis={{ rows: 3 }}>
-            Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Perferendis iste odio qui
-            voluptas temporibus neque ex modi vero corrupti
-            totam dolorem consectetur dolores porro saepe
-            cumque velit ad commodi fuga ipsam, rerum dolor
-            sunt laborum? Temporibus, saepe unde ratione,
-            laudantium a nihil dolor id architecto
-            consequuntur atque, voluptas labore. Eaque
-            repudiandae repellendus dolores impedit magni
-            cupiditate velit eligendi quaerat. Nemo,
-            delectus maiores dicta ex fugit soluta beatae
-            praesentium porro voluptates molestiae nobis
-            voluptate impedit quibusdam temporibus.
-            Exercitationem tempora nulla molestiae possimus
-            explicabo nesciunt culpa esse, aliquam
-            necessitatibus quae error cum eius reiciendis,
-            est asperiores beatae porro eveniet dolores.
-            Officia, nostrum!
-          </Paragraph>
+        <Col span={24}>
+          <Table
+            dataSource={users}
+            columns={[
+              {
+                dataIndex: 'name',
+                title: 'Nome',
+              },
+              {
+                dataIndex: 'email',
+                title: 'E-mail',
+              },
+            ]}
+          />
         </Col>
       </Row>
     </div>
