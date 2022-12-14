52a25b59cb81297dd9971e3f82a060696c0bfe93
Daniel Gomes <danielgomes@dginfope.com.br>
2022-12-13 22:47:31 -0300

    12.9. Responsividade com Breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,19 @@ function App() {
   return (
     <div>
       <Row gutter={8} justify={'center'}>
-        <Col span={4}>
+        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
           <div className={'Column'}> Coluna 1</div>
         </Col>
-        <Col span={4}>
+        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
           <div className={'Column'}> Coluna 2</div>
         </Col>
-        <Col span={8}>
+        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
           <div className={'Column'}> Coluna 3</div>
         </Col>
-        <Col span={6}>
+        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
           <div className={'Column'}> Coluna 4</div>
         </Col>
-        <Col span={4}>
+        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
           <div className={'Column'}> Coluna 5</div>
         </Col>
       </Row>
